0270177da0322ee94081f887ab57064eb1387d23
bilger-progress <bilger.yahov@progress.com>
2019-06-19 13:41:37 +0300

    Make sure to switch environments on Firebase during the deployment process.

--- env-sample/prepare-prod-build.js
+++ env-sample/prepare-prod-build.js
@@ -1,12 +1,30 @@
+const replace = require('replace-in-file');
+const { webAppConf } = require("./firebase-web-config.json");
+
 async function firebaseWebConfig() {
-  const replace = require('replace-in-file');
   const options = {
-    files: './src/firebaseApp.js',
-    from: /dev/g,
-    to: 'prod',
+    files: './firebase-web-config.json',
+    from: [
+      new RegExp(webAppConf.apiKey),
+      new RegExp(webAppConf.authDomain),
+      new RegExp(webAppConf.databaseURL),
+      new RegExp(webAppConf.projectId),
+      new RegExp(webAppConf.storageBucket),
+      new RegExp(webAppConf.messagingSenderId),
+      new RegExp(webAppConf.appId)
+    ],
+    to: [
+      process.env['prod-apiKey'],
+      process.env['prod-authDomain'],
+      process.env['prod-databaseURL'],
+      process.env['prod-projectId'],
+      process.env['prod-storageBucket'],
+      process.env['prod-messagingSenderId'],
+      process.env['prod-appId']
+    ]
   };
   try {
-    const results = await replace(options)
+    const results = await replace(options);
     // eslint-disable-next-line no-console
     console.log('#prepare-prod-build.js.firebaseWebConfig(): Replacement results: ', results);
   }
@@ -16,4 +34,26 @@ async function firebaseWebConfig() {
   }
 }
 
+async function firebaseRC() {
+  const options = {
+    files: './.firebaserc',
+    from: [
+      new RegExp(webAppConf.projectId)
+    ],
+    to: [
+      process.env['prod-projectId']
+    ]
+  };
+  try {
+    const results = await replace(options);
+    // eslint-disable-next-line no-console
+    console.log('#prepare-prod-build.js.firebaseRC(): Replacement results: ', results);
+  }
+  catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('#prepare-prod-build.js.firebaseRC(): Error occurred:', error);
+  }
+}
+
 firebaseWebConfig();
+firebaseRC();
